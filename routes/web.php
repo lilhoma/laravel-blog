2fcbbb29fbf86b7eb133a9fee01083edde3ca6a3
Juan Gomez <gomezsandovalpablo@gmail.com>
2024-03-14 14:45:57 -0700

    unfinished newsletter

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
@@ -8,8 +9,10 @@ use App\Http\Controllers\SessionsController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
+use App\Services\Newsletter;
 use Illuminate\Database\Eloquent\QueueEntityResolver;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
@@ -23,14 +26,22 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 |
 */
 
+
+
 Route::get('/', [PostController::class, 'index'])->name('home');
+
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('post/{post:slug}/comments', [PostCommentsController::class, 'store']);
 
-Route::get('register', [RegisterController::class, 'create']) -> middleware('guest');
-Route::post('register', [RegisterController::class, 'store']) -> middleware('guest'); 
 
-Route::get('login', [SessionsController::class , 'create']) -> middleware('guest'); 
-Route::post('login', [SessionsController::class , 'store']) -> middleware('guest'); 
-Route::post('logout', [SessionsController::class , 'destroy']) -> middleware('auth'); 
+Route::post('newsletter', NewsletterController::class);
+
+
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+
+
+Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
+Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
+Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
